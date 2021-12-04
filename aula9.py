eb86c1b1865d28e485d60fcfe350f22622fb2a35
Alex Wallace <alexwallax@gmail.com>
2021-12-04 15:41:51 -0300

    Segungo commit aula9

--- aula9.py
+++ aula9.py
@@ -1,3 +1,25 @@
+# criar um método escrever arquivo
+def escrever_arquivo(texto):
+    # criar um arquivo
+    arquivo = open('teste.txt', 'w') # o 'w' escreve
+    # escrever no arquivo
+    arquivo.write(texto)
+    # fechar 
+    arquivo.close()
 
-# criar um arquivo
-open('teste.txt', 'w')
+# criar um método atualizar arquivo
+def atualizar_arquivo(texto):
+    arquivo = open('teste.txt', 'a') # o 'a' atualiza
+    arquivo.write(texto)
+    arquivo.close()
+
+# criar um método para ler o arquivo
+def ler_arquivo(nome_arquivo):
+    arquivo = open(nome_arquivo, 'r') # o 'r' ler
+    texto = arquivo.read()
+    print(texto)
+
+if __name__ == '__main__':
+    # escrever_arquivo('Primeira linha\n')
+    # atualizar_arquivo('Terceira linha\n')
+    ler_arquivo('teste.txt')
